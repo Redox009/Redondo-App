d6c95d4db56016898b64b3ed5275aef53d1c16ee
Redox009 <97345898+Redox009@users.noreply.github.com>
2022-02-28 17:19:59 -0300

    Add files via upload

---
+++ src/Components/navBar.js
@@ -0,0 +1,18 @@
+
+
+
+function navBar() {
+    return <>
+             <NavBar />
+             <h2>Bienvenidos a Ofidia!</h2>
+             <ul>
+                <li> <a class="link" href= "./promociones.html"> Promociones </a> </li>
+                <li> <a class="link" href= "./convocatorias.html"> Convocatorias </a> </li>
+                <li> <a class="link" href= "./sobrenosotros.html"> Sobre Nosotros </a> </li>
+                <li> <a class="link" href= "./contacto.html"> Contacto </a> </li>
+             </ul>
+     </>;
+}
+
+
+export default navBar
